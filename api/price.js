54a8085007ecc0ab178300e3e4a37c0e6cdd79fd
manikverma77 <verma.manik@gmail.com>
2026-03-03 13:56:31 -0500

    Update price.js

--- api/price.js
+++ api/price.js
@@ -249,6 +249,16 @@ function classifyEngineType({ engineRaw, fuelTypeRaw }) {
 
 module.exports = async (req, res) => {
   try {
+    // --- CORS (allow browser apps to call this API) ---
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    
+    if (req.method === "OPTIONS") {
+      return res.status(200).end();
+    }
+    // --- end CORS ---
+    
     if (req.method !== "POST") {
       return res.status(405).json({ error: "Use POST" });
     }
